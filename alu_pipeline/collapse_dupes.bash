6a6dbe87af022662a02740627e15a776488bae80
ayeaton <ahyeaton@gmail.com>
2018-07-06 10:01:42 -0400

    Update collapse_dupes.bash

--- alu_pipeline/collapse_dupes.bash
+++ alu_pipeline/collapse_dupes.bash
@@ -32,15 +32,14 @@ awk -v BN="${BN}" '
 
 awk -v BN="${BN}" '
     NR>0{
-        if($7 - $6 >= 300)
-                arr[$18] += $4
-                count[$18] += 1
-                chr[$18] = $5
-                start[$18] = $6
-                end[$18] = $7
-                alu[$18] = $8
-                alu_sub[$18] = $17
-                alu_strand[$18] = $10
+         arr[$18] += $4
+         count[$18] += 1
+         chr[$18] = $5
+         start[$18] = $6
+         end[$18] = $7
+         alu[$18] = $8
+         alu_sub[$18] = $17
+         alu_strand[$18] = $10
     }
     END{
         for (a in arr) {
@@ -52,15 +51,14 @@ awk -v BN="${BN}" '
 
 awk -v BN="${BN}" '
     NR>0{
-        if($7 - $6 >= 300)
-                arr[$18] += $4
-                count[$18] += 1
-                chr[$18] = $5
-                start[$18] = $6
-                end[$18] = $7
-                alu[$18] = $8
-                alu_sub[$18] = $17
-                alu_strand[$18] = $10
+        arr[$18] += $4
+        count[$18] += 1
+        chr[$18] = $5
+        start[$18] = $6
+        end[$18] = $7
+        alu[$18] = $8
+        alu_sub[$18] = $17
+        alu_strand[$18] = $10
     }
     END{
         for (a in arr) {
